1ab82fda13b868b91cb14df6a41402a5db24e904
Juxt <juxtikaw@gmail.com>
2025-08-25 22:15:50 +0800

    Directory mostly complete, fixed buttons Poppins => Roboto Mono

--- JS/Directory.js
+++ JS/Directory.js
@@ -1,5 +1,36 @@
 const DIRECTORY = [
     {
+        image  : '/Assets/Officers/CP.jpg',
+        name : 'Janine P. Elias',
+        position: 'Chairperson',
+        nickname : 'Ja9',
+        rank: 'CORE',
+        socials : {
+            instagram : {socialsLink: 'https://www.instagram.com/jn.ls__?igsh=anVsNGxqZXJ0Mjdy&utm_source=qr', username: ''},
+        }
+    },
+    {
+        image  : '/Assets/Officers/ED.jpg',
+        name : 'Eliza Clarice B. Gonzales',
+        position: 'Executive Director',
+        nickname : 'Eli',
+        rank: 'CORE',
+        socials : {
+            facebook : {socialsLink: 'https://www.facebook.com/share/1BBxG5vNxo/?mibextid=wwXIfr', username: ''},
+        }
+    },
+    {
+        image  : '/Assets/Officers/OM1.jpg',
+        name : 'Bea Coloso',
+        position: 'Operations Manager',
+        nickname : 'Bey',
+        rank: 'CORE',
+        socials : {
+            facebook : {socialsLink: 'https://www.facebook.com/share/14GNVu2KjQ7/', username: ''},
+            instagram : {socialsLink: 'https://www.instagram.com/bea_cls?igsh=MXhlY2R1ZjZkYnhyNw==', username: ''},
+        }
+    },
+    {
         image  : '/Assets/Officers/QA.jpg',
         name : 'Rene Jr. P. Amantillo',
         position: 'Quality Assurance Manager',
@@ -14,7 +45,7 @@ const DIRECTORY = [
     {
         image  : '/Assets/Officers/AQA.jpg',
         name : 'Niño Jan Michael O. Bautista',
-        position: 'Assistance Quality Assurance Manager',
+        position: 'Assistant Quality Assurance Manager',
         nickname : 'Nins',
         rank: 'CORE',
         socials : {
@@ -29,6 +60,26 @@ const DIRECTORY = [
         rank: 'CORE',
         socials : {
             instagram : {socialsLink: 'https://www.instagram.com/lanabanaiai', username: '@lanabanaiai'},
+        }
+    },
+    {
+        image  : '/Assets/Officers/AFM.jpg',
+        name : 'Christopher Lance C. Dela Cruz',
+        position: 'Assistant Finance Manager',
+        nickname : 'Lance',
+        rank: 'CORE',
+        socials : {
+            instagram : {socialsLink: 'https://www.facebook.com/christopherlance.delacruz', username: '@lanabanaiai'},
+        }
+    },
+    {
+        image  : '/Assets/Officers/CommuOTL.jpg',
+        name : 'Hannie Jane M. Castillon',
+        position: 'Community Operations Team Leader',
+        nickname : 'Han',
+        rank: 'TEAM LEADER',
+        socials : {
+            facebook : {socialsLink: 'https://www.facebook.com/share/16qE6cmUTV/', username: 'Gene Kinazo'},
         }
     },
     {
@@ -52,6 +103,27 @@ const DIRECTORY = [
         }
     },
     {
+        image  : '/Assets/Officers/ReligOTL.jpg',
+        name : 'Angela Kryzyl Kate H. Eslit',
+        position: 'Religious Operations Team Leader',
+        nickname : 'Gela • Kate',
+        rank: 'TEAM LEADER',
+        socials : {
+            facebook : {socialsLink: 'https://www.facebook.com/anjkryzylkate.eslit', username: ''},
+        }
+    },
+    {
+        image  : '/Assets/Officers/ReligCTL.jpg',
+        name : 'Jaevhinn Septuagint Nichole O. Palao',
+        position: 'Religious Compliance Team Leader',
+        nickname : 'Jaev',
+        rank: 'TEAM LEADER',
+        socials : {
+            facebook : {socialsLink: 'https://www.facebook.com/palao.jaevhinn', username: ''},
+            instagram : {socialsLink: 'https://www.instagram.com/jibbberishhh?igsh=MWR0M20wMmc2OThqOA==', username: ''},
+        }
+    },
+    {
         image  : '/Assets/Officers/EnviOTL.jpg',
         name : 'Ashley L. Bagnol',
         position: 'Environment Operations Team Leader',
@@ -59,6 +131,37 @@ const DIRECTORY = [
         rank: 'TEAM LEADER',
         socials : {
             instagram : {socialsLink: 'https://www.instagram.com/ashnaruse_/', username: '@ashnaruse_'},
+        }
+    },
+    {
+        image  : '/Assets/Officers/EnviCTL.jpg',
+        name : 'Angela May L. Barriga',
+        position: 'Environment Compliance Team Leader',
+        nickname : 'Angela',
+        rank: 'TEAM LEADER',
+        socials : {
+            instagram : {socialsLink: 'https://www.facebook.com/angelamay.barriga.3?mibextid=ZbWKwL', username: ''},
+        }
+    },
+    {
+        image  : '/Assets/Officers/SocioOTL.jpg',
+        name : 'Gwyneth F. Ariston',
+        position: 'Sociocultural Operations Team Leader',
+        nickname : 'Gwen',
+        rank: 'TEAM LEADER',
+        socials : {
+            facebook : {socialsLink: 'https://www.facebook.com/gwxniiee?mibextid=wwXIfr&mibextid=wwXIfr', username: ''},
+            instagram : {socialsLink: 'https://www.instagram.com/gwxniiee?igsh=MW50bW8zMzk1MzFtMA%3D%3D&utm_source=qr', username: ''},
+        }
+    },
+    {
+        image  : '/Assets/Officers/SocioCTL.jpg',
+        name : 'Bahandi Jenn M. Babelonio',
+        position: 'Sociocultural Compliance Team Leader',
+        nickname : 'Jenn',
+        rank: 'TEAM LEADER',
+        socials : {
+            facebook : {socialsLink: 'https://www.facebook.com/share/1E486aXaQ5/?mibextid=wwXIfr', username: ''},
         }
     },
     {
@@ -81,6 +184,56 @@ const DIRECTORY = [
         socials : {
             facebook : {socialsLink: 'https://wwww.facebook.com/lovepriestes', username: 'Jan Marynoie Barcebal'},
             instagram : {socialsLink: 'https://wwww.instagram.com/potassiumpermerianganate ', username : '@potassiumpermerianganate'},
+        }
+    },
+    {
+        image  : '/Assets/Officers/MD.jpg',
+        name : 'Stella Jasmine M. Laud',
+        position: 'Membership Director',
+        nickname : 'Stell',
+        rank: 'PROJECT HEAD',
+        socials : {
+            facebook : {socialsLink: 'https://www.facebook.com/stellajasmine.laud', username: ''},
+        }
+    },
+    {
+        image  : '/Assets/Officers/TD.jpg',
+        name : 'Harvhey G. Plaza',
+        position: 'Technical Director',
+        nickname : 'Herbs',
+        rank: 'PROJECT HEAD',
+        socials : {
+            facebook : {socialsLink: 'https://www.facebook.com/harvhey.plaza?', username: ''},
+        }
+    },
+    {
+        image  : '/Assets/Officers/PodCARES.jpg',
+        name : 'Joey Adrianne A. Bayonla',
+        position: 'PodCARES Director',
+        nickname : 'Jo',
+        rank: 'PROJECT HEAD',
+        socials : {
+            facebook : {socialsLink: 'https://www.facebook.com/share/1BG2M7udJ1/', username: ''},
+        }
+    },
+    {
+        image  : '/Assets/Officers/PC1.jpg',
+        name : 'Jhon Alcel M. Salamanca',
+        position: 'Property Custodian',
+        nickname : 'Cel',
+        rank: 'PROJECT HEAD',
+        socials : {
+            facebook : {socialsLink: 'https://www.facebook.com/share/16th1jHeKh/', username: ''},
+        }
+    },
+    {
+        image  : '/Assets/Officers/PC2.jpg',
+        name : 'Jeoffery Mar D. Olmido',
+        position: 'Property Custodian',
+        nickname : 'Jeo',
+        rank: 'PROJECT HEAD',
+        socials : {
+            facebook : {socialsLink: 'https://www.facebook.com/share/1GCqQKwc6h/', username: ''},
         }
     },
 ];
@@ -113,29 +266,28 @@ THE_OFFICERS.innerHTML = `${content}
 
 function directoryToClass(position) {
     let key = {
-        'Chairperson' : 'cp',
-        'Executive Director' : 'ed',
-        'Operations Manager' : 'om',
-        'Quality Assurance Manager' : 'qa',
-        'Assistance Quality Assurance Manager' : 'aqa',
-        'Finance Manager' : 'fm',
-        'Assistance Finance Manager' : 'afm',
-        'Communications Director' : 'cd',
-        'Membership Director' : 'md',
-        'Technical Director' : 'td',
-        'PodCARES Director' : 'podcares',
-        'Property Custodians' : 'pc',
-        'Community Operations Team Leader' : 'commu-otl',
-        'Community Compliance Team Leader' : 'commu-ctl',
-        'Community Operations Team Leader' : 'commu-otl',
-        'Academics Operations Team Leader' : 'acad-otl',
-        'Academics Compliance Team Leader' : 'acad-ctl',
-        'Religious Operations Team Leader' : 'relig-otl',
-        'Religious Compliance Team Leader' : 'relig-ctl',
-        'Environment Operations Team Leader' : 'envi-otl',
-        'Environment Compliance Team Leader' : 'envi-ctl',
-        'Sociocultural Operations Team Leader' : 'socio-otl',
-        'Sociocultural Compliance Team Leader' : 'socio-ctl',
+        'Chairperson' : 'cp', //done
+        'Executive Director' : 'ed', //done
+        'Operations Manager' : 'om', //partial
+        'Quality Assurance Manager' : 'qa', //done
+        'Assistant Quality Assurance Manager' : 'aqa', //done
+        'Finance Manager' : 'fm', //done
+        'Assistant Finance Manager' : 'afm', //done
+        'Community Operations Team Leader' : 'commu-otl', //done
+        'Community Compliance Team Leader' : 'commu-ctl', //done
+        'Academics Operations Team Leader' : 'acad-otl', //done
+        'Academics Compliance Team Leader' : 'acad-ctl', //done
+        'Religious Operations Team Leader' : 'relig-otl', //done
+        'Religious Compliance Team Leader' : 'relig-ctl', //done
+        'Environment Operations Team Leader' : 'envi-otl', //done
+        'Environment Compliance Team Leader' : 'envi-ctl', //done
+        'Sociocultural Operations Team Leader' : 'socio-otl', //done
+        'Sociocultural Compliance Team Leader' : 'socio-ctl', //done
+        'Communications Director' : 'cd', //done
+        'Membership Director' : 'md', //done
+        'Technical Director' : 'td', //done
+        'PodCARES Director' : 'podcares', //done
+        'Property Custodian' : 'pc', //done
     }
 
     return key[position]
